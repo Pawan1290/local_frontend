63c579b31ab0e27ec5068d433e265fe25f19ffab
pawan1290 <pawan199890@gmail.com>
2020-05-31 12:53:30 +0530

    Completed logout, login and authentication.

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private baseUrl = environment.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  signup(data) {
+    return this.http.post(`${this.baseUrl}/register`, data);
+  }
+
+  login(data) {
+    return this.http.post(`${this.baseUrl}/login`, data);
+  }
+}
---
+++ src/app/services/auth1.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { TokenService } from './token.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Auth1Service {
+
+  private loggedIn = new BehaviorSubject<boolean>(this.Token.loggedIn());
+  authStatus = this.loggedIn.asObservable();
+
+  changeAuthStatus(value: boolean) {
+    this.loggedIn.next(value);
+  }
+
+  constructor(private Token: TokenService) { }
+}
